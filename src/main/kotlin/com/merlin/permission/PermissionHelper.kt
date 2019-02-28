64dfd2bb479bedf0a6728cc31c4acd96ba2e351b
赵安林 <zhaoal@cnsuning.com>
2019-02-28 14:19:48 +0800

    fix依赖

--- src/main/kotlin/com/merlin/permission/PermissionHelper.kt
+++ src/main/kotlin/com/merlin/permission/PermissionHelper.kt
@@ -71,7 +71,7 @@ open class PermissionHelper private constructor() {
         val intent = Intent()
         intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
         intent.addCategory(Intent.CATEGORY_DEFAULT)
-        intent.data = Uri.parse("package:${context?.packageName}")
+        intent.data = Uri.parse("package:${context.packageName}")
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
         intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
